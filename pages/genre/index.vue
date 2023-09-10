30a9053e92d7586f2281845de21569d1c412ff9f
iqbal ardiansyah <dibul250899@gmail.com>
2023-09-10 23:01:35 +0700

    UPDATE title genre page

--- pages/genre/index.vue
+++ pages/genre/index.vue
@@ -8,6 +8,11 @@
   </div>
 </template>
 
-<script></script>
+<script lang="ts" setup>
+//meta title
+useHead({
+  title: "Noveltoon - Clone | Genre",
+});
+</script>
 
 <style lang="scss" scoped></style>

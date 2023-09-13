f29c92d429a99968d3cde9646153f97a4929671d
iqbal ardiansyah <dibul250899@gmail.com>
2023-09-13 22:19:34 +0700

    UPDATE page/read comment section

--- components/Read/Content.vue
+++ components/Read/Content.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="w-full flex justify-center">
-    <div class="w-11/12 md:w-2/3 flex flex-col items-center border mt-7 md:mt-16 mb-16 pb-10">
+  <div class="w-full flex justify-center pb-4">
+    <div
+      class="w-11/12 md:w-2/3 flex flex-col items-center border mt-7 md:mt-16 mb-0 md:mb-16 pb-10"
+    >
       <div class="md:pt-16 pt-8">
-        <img src="https://picsum.photos/200/300" alt="" class="rounded-xl h-36 md:h-64" />
+        <img
+          src="https://picsum.photos/200/300"
+          alt=""
+          class="rounded-xl h-36 md:h-64"
+        />
       </div>
       <div class="font-semibold text-sm md:text-xl md:pt-8 pt-4">
         <p>{{ readNovelData[0].title }}</p>
@@ -15,6 +21,106 @@
         v-html="readNovelData[0].story"
         class="text-base md:text-2xl px-10 md:px-20 text-justify"
       ></div>
+      <div class="flex content-center space-x-8">
+        <div class="p-4 border rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+            />
+          </svg>
+        </div>
+        <div class="p-4 border rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="pt-4">
+        <button class="py-3 px-6 bg-blue-600 rounded-full text-white pt-4">
+          Download Novel PDF
+        </button>
+      </div>
+      <div class="w-full">
+        <div class="px-20 font-semibold">
+          <p>Comments</p>
+        </div>
+      </div>
+      <div class="w-full flex px-20 pt-8">
+        <div class="">
+          <img
+            src="https://picsum.photos/200/300"
+            class="h-10 w-10 object-cover rounded-full"
+            alt=""
+          />
+        </div>
+        <div class="flex flex-col ml-8 w-full">
+          <p class="font-semibold tex-lg">Rapa Rasha</p>
+          <p class="pt-4">
+            "Kemenangan terbesar adalah pergi dan memilih untuk tidak terlibat
+            dalam drama dan energi beracun sama sekali."
+          </p>
+          <div class="flex flex-wrap justify-between">
+            <p class="text-gray-400">2023-08-19</p>
+            <div class="flex">
+              <div class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                  />
+                </svg>
+                <p class="pl-2">Komen</p>
+              </div>
+              <div class="flex pl-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+                  />
+                </svg>
+
+                <p class="pl-2">Like</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
